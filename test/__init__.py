dffe7c2aad08657f84d1ed7dfb3eae25ce37a7ca
zueves@gmail.com <zueves@gmail.com@14c53a28-ea53-75df-6c54-14096e524f45>
2014-02-11 17:21:45 +0000

    Add doctests to unit tests suite

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,12 @@
 ﻿import unittest
+import doctests
 from trans import TestTrans
 from error import TestError
 
-suite = unittest.TestSuite()
+#suite = unittest.TestSuite()
+suite = unittest.TestLoader().loadTestsFromModule(doctests)
 suite.addTest(unittest.makeSuite(TestTrans))
 suite.addTest(unittest.makeSuite(TestError))
 
-unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- test/doctests.py
+++ test/doctests.py
@@ -2,7 +2,11 @@ import neurolab as nl
 import doctest
 import inspect
 
-for m in dir(nl):
-    m = eval('nl.' + m)
-    if inspect.ismodule(m):
-        doctest.testmod(m, verbose=True)
+def load_tests(loader, tests, ignore):
+    for m in dir(nl):
+        m = eval('nl.' + m)
+        if inspect.ismodule(m):
+            doctest.testmod(m, verbose=True)
+            tests.addTests(doctest.DocTestSuite(m))
+    return tests
+
