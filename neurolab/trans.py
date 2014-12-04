b4af4d73ec679bc9b3c534c50f08a993b6e7ce82
zueves <zueves@14c53a28-ea53-75df-6c54-14096e524f45>
2014-12-04 17:04:41 +0000

    fix spaces

--- neurolab/trans.py
+++ neurolab/trans.py
@@ -104,7 +104,7 @@ class LogSig:
     inp_active = [-4, 4]
 
     def __call__(self, x):
-        return 1/(1+np.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     def deriv(self, x, y):
         """
